64c74f90c49dd8f3da37de32fbde2f93a3fd18ae
jorliano <jorliano@hotmail.com>
2018-02-01 19:02:09 -0200

    Adicionado API para consumir dados de filmes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
@@ -12,6 +13,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { FeedPageModule } from '../pages/feed/feed.module';
 import { IntroPageModule } from '../pages/intro/intro.module';
+import { MovieProvider } from '../providers/movie/movie';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,7 @@ import { IntroPageModule } from '../pages/intro/intro.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpClientModule,
     FeedPageModule,
     IntroPageModule
   ],
@@ -38,7 +41,8 @@ import { IntroPageModule } from '../pages/intro/intro.module';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MovieProvider
   ]
 })
 export class AppModule {}
--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MovieProvider } from '../../providers/movie/movie';
 
 /**
  * Generated class for the FeedPage page.
@@ -12,14 +13,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  provider: [
+    MovieProvider
+  ]
 })
 export class FeedPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  object_feed = {
+    titulo: "Nova noticia",
+    date: "Novemver 5, 2000",
+    descricao: "Estou criando um app incrivel",
+    qntd_likes: 10,
+    qntd_comments: 4,
+    time_comment: "11h ago"
   }
 
+  public list_filmes = new Array<any>();
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private movieProvider: MovieProvider) {
+  }
+
+
+
   ionViewDidLoad() {
+    this.movieProvider.getLatestMovies().subscribe(
+      data =>{
+        console.log(data.results);           
+        this.list_filmes = data.results;
+      },erro => {
+        console.log(erro);
+      });
+
     console.log('ionViewDidLoad FeedPage');
   }
+
 
 }
